18813d4747e9f5ff33d81413f879a26ace41bc43
Yura <macho19972010@mail.ru>
2016-11-21 20:21:35 +0200

    Added WrongValueTypeException and Position class

---
+++ DataBaseLibrary/CustomExceptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataBaseLibrary
+{
+    public class WrongValueTypeException : Exception
+    {
+        public WrongValueTypeException(string message = "Wrong type of value. Please use numeric values(integer, double, decimal).")
+            : base(message)
+        {
+
+        }
+    }
+}
---
+++ DataBaseLibrary/Position.cs
@@ -0,0 +1,19 @@
+﻿namespace DataBaseLibrary
+{
+    public class Position<T> where T : struct
+    {
+        public IPoint Point { get; }
+
+        public T Value { get; }
+
+        public Position(IPoint point, T value)
+        {
+            var typeOfValue = value.GetType();
+            if ( typeOfValue.IsEnum || typeOfValue == typeof(bool) )
+                throw new WrongValueTypeException();
+
+            Point = point;
+            Value = value;
+        }
+    }
+}
